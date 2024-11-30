1641f842e9254225f92228c1947c31a502646ac0
Finnley Grange <per.finnleygrange@icloud.com>
2024-11-30 12:08:35 +0000

    Platform spawn + move up

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject spawner;
+    [SerializeField] GameObject spawnerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawn(2, spawnerPrefab));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public IEnumerator Spawn(float time, GameObject platform)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            Instantiate(platform, spawner.transform.position, spawner.transform.rotation);
+        }
     }
 }
